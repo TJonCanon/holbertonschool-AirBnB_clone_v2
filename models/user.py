e1fc8d286374a0c62781253ebf0702f1fe87881b
TJonCanon <taylor.joyner93@me.com>
2023-03-07 21:55:17 -0600

    task 8

--- models/user.py
+++ models/user.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-import os
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+""" State Module for HBNB project """
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
-    """
-    __tablename__ = "users"
-
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
+    reviews = relationship(
+        "Review",
+        backref='user',
+        cascade="all, delete",
+        passive_deletes=True)
