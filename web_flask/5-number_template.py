af902d2490f404e1f5315d32aeab73d8be6c9c22
TJonCanon <taylor.joyner93@me.com>
2023-03-27 22:21:02 -0500

    task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Starts a flash web application """
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is_cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def numb(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def numbtemp(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
