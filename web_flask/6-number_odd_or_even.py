86cf66982a4005b8930d8323f02870459e46b231
TJonCanon <taylor.joyner93@me.com>
2023-03-27 22:47:08 -0500

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,7 +41,7 @@ def even_or_odd(n):
     x = "even" if n % 2 == 0 else "odd"
     return render_template(
         '6-number_odd_or_even.html', n=n, x=x
-
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
